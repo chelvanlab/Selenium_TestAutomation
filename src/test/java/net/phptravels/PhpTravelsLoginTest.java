cc3b001d40557aaad57abd15f8f2a43d8927c602
chelvanai <chelvamchelvan@yahoo.com>
2020-09-27 10:48:25 +0530

    dev

--- src/test/java/net/phptravels/PhpTravelsLoginTest.java
+++ src/test/java/net/phptravels/PhpTravelsLoginTest.java
@@ -1,7 +1,10 @@
 package net.phptravels;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,7 +21,21 @@ public class PhpTravelsLoginTest {
     }
 
     @Test
-    public void testLogin() {
+    public void testLogin() throws InterruptedException {
+        driver.findElement(By.name("email")).sendKeys("admin@phptravels.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demoadmin");
+        driver.findElement(By.xpath(("//*[@type='submit']"))).click();
 
+        Thread.sleep(10000);
+        String message = driver.findElement(By.xpath("//*[contains(text(),'Hi Admin!')]")).getText();
+
+        Assert.assertEquals(message,"Hi Admin!");
+        driver.findElement(By.xpath("//*[contains(text(),'Logout')]")).click();
+
+    }
+
+    @AfterMethod
+    public void tearDwon(){
+        driver.quit();
     }
 }
